75319b3ac6c1ca3912bcf3a00b2548da47e00673
drewcoder <drewcoder@gmail.com>
2022-02-19 13:37:25 -0500

    Initial Commit

---
+++ Game Template tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Game Template tvOS
+//
+//  Created by Ritchie Andrews on 2/19/22.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
